461e25777ccc1ff4c29ce8be0dde2dbbea3d0b49
Anderson Camacho <hadoop@AND.localdomain>
2026-03-05 23:20:32 -0500

    Corrigiendo app_name de las actividades spark

--- datalake/procesos/poblar_capa_functional.py
+++ datalake/procesos/poblar_capa_functional.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 """
 Script PySpark para despliegue de capa Functional (Parquet) - Detección de Diabetes
 Proyecto: topicos-deteccion-diabetes
